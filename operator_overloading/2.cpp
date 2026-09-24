26b9fdce84295f3aed8146623ba65399661f118f
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-09-24 22:54:41 +0530

    2.OO

---
+++ operator_overloading/2.cpp
@@ -0,0 +1,38 @@
+// Operator overloading of unary operator -- using member function
+
+#include <iostream>
+using namespace std;
+
+class Nums
+{
+    int x, y, z;
+
+public:
+    Nums(int n1 = 0, int n2 = 0, int n3 = 0)
+    {
+        x = n1;
+        y = n2;
+        z = n3;
+    }
+    void display()
+    {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+        cout << "z = " << z << endl;
+    }
+    int operator--()
+    {
+        x--;
+        y--;
+        z--;
+    }
+};
+
+int main()
+{
+    Nums n(5, 3, 6);
+    n.display();
+    --n;
+    n.display();
+    return 0;
+}
