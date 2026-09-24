f4004f69be7b17266484cff7c12507b88a522484
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-09-24 22:49:07 +0530

    1.OO

---
+++ operator_overloading/1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Num
+{
+    int n;
+
+public:
+    Num(int x = 0)
+    {
+        n = x;
+    }
+    void display()
+    {
+        cout << "n = " << n << endl;
+    }
+    friend void operator-(Num &num);
+};
+
+void operator-(Num &num)
+{
+    num.n = -num.n;
+}
+
+int main()
+{
+    Num num(10);
+    num.display();
+    -num;
+    num.display();
+
+    return 0;
+}
